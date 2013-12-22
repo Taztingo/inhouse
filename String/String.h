2e153aed4c830149ec777d549de48edc4f9c7641
Matt Witkowski <mwitkows@stevens.edu>
2013-12-22 01:01:47 -0500

    Put string in namespace inhouse.

--- String/String.h
+++ String/String.h
@@ -6,38 +6,42 @@
 #ifndef STRING_H_
 	#define STRING_H_
 
-	class String
+	namespace inhouse
 	{
-		private:
-			unsigned int _length;
-			char* _string;
+		class String
+		{
+			private:
+				unsigned int _length;
+				char* _string;
 
-		public:
-			String();
-			String(char[] string);
+			public:
+				String();
+				String(char[] string);
+				~String();
 			
-			char charAt(int index);
-			int compareTo(String& string);
-			int compareToIgnoreCase(String& string);
-			bool contains(String& string);
-			int indexOf(char character);
-			int indexOf(String& string);
-			bool isEmpty();
-			int lastIndexOf(char character);
-			int lastIndexOf(String& string);
-			int length();
-			String& substring(int begin, int end);
-			char[] toCharArray();
-			String& toLowerCase();
-			String& toString();
-			String& toUpperCase();
-			String& trim();
+				char charAt(int index);
+				int compareTo(String& string);
+				int compareToIgnoreCase(String& string);
+				bool contains(String& string);
+				int indexOf(char character);
+				int indexOf(String& string);
+				bool isEmpty();
+				int lastIndexOf(char character);
+				int lastIndexOf(String& string);
+				int length();
+				String& substring(int begin, int end);
+				char[] toCharArray();
+				String& toLowerCase();
+				String& toString();
+				String& toUpperCase();
+				String& trim();
 
-			bool operator==(String& string);
-			String& operator=(String& string);
-			String& operator+(String& string);
-			String& operator+(String& integer);
-			String& operator+(bool boolean);
-	};
+				bool operator==(String& string);
+				String& operator=(String& string);
+				String& operator+(String& string);
+				String& operator+(String& integer);
+				String& operator+(bool boolean);
+		};
+	}
 
 #endif
