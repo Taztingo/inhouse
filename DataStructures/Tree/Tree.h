589f40c2f69934e935c943002385368a9133271d
mwitkows <mwitkows@stevens.edu>
2014-01-12 22:34:24 -0500

    Created empty functions for all methods of Tree.

--- DataStructures/Tree/Tree.h
+++ DataStructures/Tree/Tree.h
@@ -55,18 +55,66 @@
 		Tree<T>::~Tree()
 		{
 		}
-				T& getChild(uint nodeIndex, uint child);
-				void setChild(uint nodeIndex, uint child, T& element);
-				String toString();
-				uint getSize(uint nodeIndex);
-				String traverseInOrder();
-				String traversePreOrder();
-				String traversePostOrder();
-				T& removeChild(uint nodeIndex, uint child);
-				bool isLeaf();
-				operator=(Tree& tree);
-				operator==(Tree& tree);
-				operator!=(Tree& tree);
+		
+		template <typename T>
+		T& Tree<T>::getChild(uint nodeIndex, uint child)
+		{
+		}
+
+		template <typename T>
+		void Tree<T>::setChild(uint nodeIndex, uint child, T& element)
+		{
+		}
+
+		template <typename T>
+		String Tree<T>::toString()
+		{
+		}
+
+		template <typename T>
+		uint Tree<T>::getSize(uint nodeIndex)
+		{
+		}
+
+		template <typename T>
+		String Tree<T>::traverseInOrder()
+		{
+		}
+
+		template <typename T>
+		String Tree<T>::traversePreOrder()
+		{
+		}
+
+		template <typename T>
+		String Tree<T>::traversePostOrder()
+		{
+		}
+
+		template <typename T>
+		T& Tree<T>::removeChild(uint nodeIndex, uint child);
+		{
+		}
+		
+		template <typename T>
+		bool Tree<T>::isLeaf()
+		{
+		}
+
+		template <typename T>
+		bool Tree<T>::operator=(Tree& tree)
+		{
+		}
+
+		template <typename T>
+		bool Tree<T>::operator==(Tree& tree)
+		{
+		}
+
+		template <typename T>
+		bool Tree<T>::operator!=(Tree& tree)
+		{
+		}
 	}
 
 #endif
