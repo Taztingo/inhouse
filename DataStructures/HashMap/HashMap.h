3fa368c70bdb904f12fb2ee00dc8e841d5dd5c1c
mwitkows <mwitkows@stevens.edu>
2014-10-15 23:58:32 -0400

    Added Skeleton for Open HashMap and Pair.

---
+++ DataStructures/HashMap/HashMap.h
@@ -0,0 +1,104 @@
+#ifndef HASHMAP_H_
+#define HASHMAP_H_
+
+#include "../../Common/Type.h"
+#include "../DynamicArray/DynamicArray.h"
+#include "Pair.h"
+
+namespace inhouse
+{
+	
+	template <class K, class V>
+	class HashMap
+	{
+		public:
+			HashMap();
+			HashMap(uint capacity);
+			HashMap(HashMap<K,V>& hashMap);
+			~HashMap();
+			void clear();
+			bool containsKey(K& key);
+			bool containsValue(V& value);
+			V& remove(K& key);
+			uint size();
+
+			bool operator==(HashMap<K,V>& hashMap);
+			HashMap& operator=(HashMap<K,V>& hashMap);
+			V& operator[](K& key);
+
+		private:
+			uint _size;
+			DynamicArray<DynamicArray<Pair<K,V> > > _hashMap;
+	};
+
+	template <class K, class V>
+	HashMap<K,V>::HashMap()
+	{
+	}
+
+	template <class K, class V>
+	HashMap<K,V>::HashMap(uint capacity)
+	{
+	}
+
+	template <class K, class V>
+	HashMap<K,V>::HashMap(HashMap<K,V>& hashMap)
+	{
+	}
+
+	template <class K, class V>
+	HashMap<K,V>::~HashMap()
+	{
+	}
+	
+	template <class K, class V>
+	void HashMap<K,V>::clear()
+	{
+	}
+	
+	template <class K, class V>
+	bool HashMap<K,V>::containsKey(K& key)
+	{
+		return true;
+	}
+	
+	template <class K, class V>
+	bool HashMap<K,V>::containsValue(V& value)
+	{
+		return true;
+	}
+	
+	template <class K, class V>
+	V& HashMap<K,V>::remove(K& key)
+	{
+		V v;
+		return v;
+	}
+	
+	template <class K, class V>
+	uint HashMap<K,V>::size()
+	{
+		return 0;
+	}
+	
+	template <class K, class V>
+	bool HashMap<K,V>::operator==(HashMap<K,V>& hashMap)
+	{
+		return true;
+	}
+
+	template <class K, class V>
+	HashMap<K,V>& HashMap<K,V>::operator=(HashMap<K,V>& hashMap)
+	{
+		return *this;
+	}
+
+	template <class K, class V>
+	V& HashMap<K,V>::operator[](K& key)
+	{
+		V v;
+		return v;
+	}
+}
+
+#endif
---
+++ DataStructures/HashMap/Pair.h
@@ -0,0 +1,43 @@
+#ifndef PAIR_H_
+#define PAIR_H_
+
+namespace inhouse
+{
+	template <class K, class V>
+	struct Pair
+	{
+		public:
+			Pair(K& key, V& value);
+			~Pair();
+
+			K& getKey();
+			V& getValue();
+
+		private:
+			K _key;
+			V _value;
+	};
+
+	template <class K, class V>
+	Pair<K,V>::Pair(K& key, V& value)
+	{
+	}
+
+	template <class K, class V>
+	Pair<K,V>::~Pair()
+	{
+	}
+	
+	template <class K, class V>
+	K& Pair<K,V>::getKey()
+	{
+		return _key;
+	}
+	
+	template <class K, class V>
+	V& Pair<K,V>::getValue()
+	{
+		return _value;
+	}
+}
+#endif
